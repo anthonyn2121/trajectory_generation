29bba6e72b5449004845372266b85e84ba304d72
Anthony <anthonyn2121@gmail.com>
2024-08-27 07:06:02 -0400

    better formatting

--- trajectory_generator.py
+++ trajectory_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def filter_points(waypoints):
     waypoints = np.asarray(waypoints)
     # Initialize list of necessary waypoints
@@ -22,21 +23,22 @@ def filter_points(waypoints):
     necessary_waypoints.append(waypoints[-1])
     return np.array(necessary_waypoints)
 
+
 class PolyTraj(object):
     def __init__(self, r, waypoints):
-        '''
+        """
         Creates a piecewise polynomial spline to generate trajectories of an entity
 
                                | P1(t), t0 <= t <= t1
                         P(t) = | P2(t), t1 <= t <= t2
                                | P3(t), t2 <= t <= t3
-                               
+
         Args:
             r (int): The order of the derivative to minimize
-            waypoints (list, np.array): The waypoints the entity should pass through            
-        '''
+            waypoints (list, np.array): The waypoints the entity should pass through
+        """
         self.r = r  ## order to minimize
-        self.n = 2*r - 1  ## order of polynomial
+        self.n = 2 * r - 1  ## order of polynomial
         self.waypoints = filter_points(waypoints)
         self.num_waypoints = self.waypoints.shape[0]
         self.num_segments = self.num_waypoints - 1
@@ -44,11 +46,14 @@ class PolyTraj(object):
 
         ## Construct an array of time values where the values correspond to
         ## the time it should take the agent to reach that waypoint
-        distances = np.zeros((self.waypoints.shape[0] - 1,3))
+        ## this is an arbitrary value, not definitive
+        distances = np.zeros((self.waypoints.shape[0] - 1, 3))
         for i in range(distances.shape[0]):
-            distances[i] =  self.waypoints[i+1] - self.waypoints[i]
+            distances[i] = self.waypoints[i + 1] - self.waypoints[i]
         self.times = np.cumsum(np.linalg.norm(distances, axis=1))
-        self.times = np.append(np.array([[0]]), self.times)  ## Insert 0 at the beginning of self.times
+        self.times = np.append(
+            np.array([[0]]), self.times
+        )  ## Insert 0 at the beginning of self.times
         self.times /= 2
 
         ## Find the 3D rth-derivative of position
@@ -56,12 +61,14 @@ class PolyTraj(object):
         for r in range(1, self.r):
             dr_array = np.zeros(self.waypoints.shape)
             for i in range(self.waypoints.shape[0] - 1):
-                if (r == 1):
-                    drdt = (self.waypoints[i+1] - self.waypoints[i]) / self.times[i+1]
-                    dr_array[i+1] = drdt
+                if r == 1:
+                    drdt = (self.waypoints[i + 1] - self.waypoints[i]) / self.times[
+                        i + 1
+                    ]
+                    dr_array[i + 1] = drdt
                 else:
-                    drdt = (self.d[r-1][i + 1] - self.d[r-1][i]) / self.times[i+1]
-                    dr_array[i+1] = drdt
+                    drdt = (self.d[r - 1][i + 1] - self.d[r - 1][i]) / self.times[i + 1]
+                    dr_array[i + 1] = drdt
             self.d[r] = dr_array
 
         for k in self.d.keys():
@@ -79,11 +86,13 @@ class PolyTraj(object):
         Return:
             np.array or None: Position at time 't' or No update
         """
-        self.flat_output = {'x': np.zeros(3),          ## position
-                            'x_dot': np.zeros(3),      ## velocity
-                            'x_ddot': np.zeros(3),     ## acceleration
-                            'x_dddot': np.zeros(3),    ## jerk
-                            'x_ddddot': np.zeros(3)}   ## snap
+        self.flat_output = {
+            "x": np.zeros(3),  ## position
+            "x_dot": np.zeros(3),  ## velocity
+            "x_ddot": np.zeros(3),  ## acceleration
+            "x_dddot": np.zeros(3),  ## jerk
+            "x_ddddot": np.zeros(3),  ## snap
+        }
 
         for i, time in enumerate(self.times):
             if t < time:
@@ -107,29 +116,33 @@ class PolyTraj(object):
             list: A list of values after substituting in 't', before summation
         """
         basis = np.zeros(self.num_coefficients)
-        temp = np.array([t**i for i in range(self.num_coefficients-order-1, -1, -1)])
-        basis[:len(temp)] = temp
+        temp = np.array(
+            [t**i for i in range(self.num_coefficients - order - 1, -1, -1)]
+        )
+        basis[: len(temp)] = temp
         return basis
-    
+
     def __basis_coefficients(self, order=0):
         """
         Returns the coefficients of a polynomial function or its derivative of a specified order.
-        
+
         Args:
             coefficients (list): A list of coefficients representing the polynomial function.
             order (int, optional): The order of the derivative to compute. Default is 0 (original polynomial).
-        
+
         Returns:
             list: A list of coefficients for the polynomial function or its derivative of the specified order.
-        """            
+        """
         coefficients = np.ones(self.n + 1)
         for _ in range(order):
             # Compute the derivative coefficients
             degree = len(coefficients) - 1
             derivative_coeffs = [coefficients[i] * i for i in range(1, degree + 1)]
             coefficients = derivative_coeffs
-        
-        return np.append(coefficients[::-1], np.zeros(self.num_coefficients - len(coefficients)))
+
+        return np.append(
+            coefficients[::-1], np.zeros(self.num_coefficients - len(coefficients))
+        )
 
     def __set_constraints(self, T, start, end):
         """
@@ -154,31 +167,35 @@ class PolyTraj(object):
         b[1::2] = end.reshape((-1, 1))
 
         ## position constraints
-        A[:2, :] = np.vander([0, T], N = self.n + 1)
+        A[:2, :] = np.vander([0, T], N=self.n + 1)
 
         ## higher order derivative constraints
         order = 2
         row = 2
-        for j in range((self.num_coefficients - 2)//2):
+        for j in range((self.num_coefficients - 2) // 2):
             order = j + 1
-            A[row, -order-1] = np.polyder(np.poly1d([1]*self.num_coefficients), order)(0)
-            A[row+1, :] = self.__basis_coefficients(order) * self.__polynomial_basis(T, order)
+            A[row, -order - 1] = np.polyder(
+                np.poly1d([1] * self.num_coefficients), order
+            )(0)
+            A[row + 1, :] = self.__basis_coefficients(order) * self.__polynomial_basis(
+                T, order
+            )
             row += 2
 
         return A, b
-    
+
     def __solve_polynomials(self):
         """
-        Finds the coefficients of each polynomial function found for every segment of the trajectory by 
-        solving the Ap = b equation and finding A,b matrices from __set_constraints            
+        Finds the coefficients of each polynomial function found for every segment of the trajectory by
+        solving the Ap = b equation and finding A,b matrices from __set_constraints
         """
         coeffs = []
         for i in range(self.num_segments):
             p = []
             for j in range(3):
-                T = self.times[i+1]  ## time to reach next waypoint
+                T = self.times[i + 1]  ## time to reach next waypoint
                 start = [self.waypoints[i, j]]
-                end = [self.waypoints[i+1, j]]
+                end = [self.waypoints[i + 1, j]]
                 for k in self.d.keys():
                     start.append(self.d[k][i, j])
                     end.append(self.d[k][i + 1, j])
@@ -199,12 +216,13 @@ class PolyTraj(object):
             float: The dot product between both vectors
         """
         return self.__polynomial_basis(t) @ coeffs
-    
+
     def __get_flat_output(self, t, coeffs, axis):
         for i in range(len(self.d) + 1):
             key = list(self.flat_output.keys())[i]
-            self.flat_output[key][axis] = (self.__basis_coefficients(order=i) * self.__polynomial_basis(t, order=i)) @ coeffs
-
+            self.flat_output[key][axis] = (
+                self.__basis_coefficients(order=i) * self.__polynomial_basis(t, order=i)
+            ) @ coeffs
 
         return self.flat_output
 
@@ -217,24 +235,31 @@ if __name__ == "__main__":
     positions = []
     for t in time_span:
         flat_output = traj.update(t)
-        if (flat_output == None):
+        if flat_output == None:
             pos = positions[-1]
         else:
-            pos = flat_output['x']
+            pos = flat_output["x"]
         positions.append(pos)
 
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
+
     trajectory = np.asarray(positions).reshape((len(time_span), 3))
 
     # Plotting the trajectory and waypoints
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], label='Trajectory')
-    ax.scatter(waypoints[:, 0], waypoints[:, 1], waypoints[:, 2], color='red', label='Waypoints')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2], label="Trajectory")
+    ax.scatter(
+        waypoints[:, 0],
+        waypoints[:, 1],
+        waypoints[:, 2],
+        color="red",
+        label="Waypoints",
+    )
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
     ax.legend()
     plt.savefig("mytrajectory.png")
     plt.show(block=True)
